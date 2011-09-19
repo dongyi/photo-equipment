9dc5de6b3347080969f62bcc9d6e8cd25536e51f
dongyi <juvenpp@gmail.com>
2011-09-19 16:50:51 +0800

    new page

---
+++ static/js/equipment.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20110919
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('equipment_files/equipmentMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
