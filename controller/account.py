ff86b68d18e064d488f9cb881c502d62b6c8bfa6
dongyi <dongyi@dongyi.com>
2011-09-29 17:42:18 +0800

    some signup login action

--- controller/account.py
+++ controller/account.py
@@ -4,14 +4,20 @@ import tornado.database
 
 from common.base_httphandler import BaseHandler
 
-def new_user(name, password, email):
-    pass
-
 def check_user(email, password):
     pass
 
+def qn(s):
+    dirty_stuff = ["\"", "\\", "/", "*", "'", "=", "-", "#", ";", "<", ">", "+", "%"]
+    for stuff in dirty_stuff:
+        s = s.replace(stuff,"")
+    return "'"+s+"'"
+
 class AccountHandler(BaseHandler):
     def get(self, action):
+        f = lambda x:self.get_argument(x, '').strip()
+        name = f('name')
+        password = f('password')
         if action == 'login':
             return self.render('login.html')
         elif action == 'signup':
@@ -22,10 +28,15 @@ class AccountHandler(BaseHandler):
             self.redirect('/')
 
     def post(self, action):
+        f = lambda x:self.get_argument(x, '').strip()
         if action == 'login':
+            user = self.db.get('select * from account where name=%s and password=PASSWORD(%s)'%(qn(name), qn(password))) # this will raise error if not exist
+            self.session['user'] = dict(name=user.name, id=user.id)
+            self.session.save()
             return self.redirect('/')
         elif action == 'signup':
-            name = self.get_argument('name')
-            email = self.get_argument('email')
-            password = self.get_argument('password')
+            name = f('name')
+            email = f('email')
+            password = f('password')
+            self.db.execute('INSERT INTO account (name, email, password) VALUES(%s, %s, MD5(%s))'%(qn(name), qn(email), qn(password)))
             return self.render('/')
